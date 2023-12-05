82d0db7771662de75ef528fc7ff1d5a911b592be
Nguyễn Kế Bảo <baonguyen301103@gmail.com>
2023-12-05 16:30:21 +0700

    Up file git

---
+++ SQl.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE Attendance
+ADD CONSTRAINT FK_Attendance_Classes_ClassID FOREIGN KEY (ClassID) REFERENCES Classes(ClassID);
+
+-- Thêm khóa ngoại vào bảng Classes
+ALTER TABLE Classes
+ADD CONSTRAINT FK_Classes_Course FOREIGN KEY (CourseID) REFERENCES Courses(CourseID);
